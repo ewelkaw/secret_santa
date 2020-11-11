adff9534606d3b1d85cf2f52da8526afe52c6779
Ewelina Szmielew <ewelina@szmielew.pl>
2020-11-11 12:54:49 +0100

    Add saving matches

--- users_app/app/jobs/generate_matchings_job.rb
+++ users_app/app/jobs/generate_matchings_job.rb
@@ -11,5 +11,13 @@ class GenerateMatchingsJob < ApplicationJob
       
     response = HTTParty.post('http://localhost:8090/generate', options)
     pp JSON.parse(response.body)
+    data_matches = JSON.parse(response.body)
+
+    # byebug
+
+    data_matches["names"].each do |match|
+      matched_user = User.find_by(secret_santa_id: secret_santa_id, name: match["to"])
+      User.find_by(secret_santa_id: secret_santa_id, name: match["from"]).update_attribute(:matched_user_id, matched_user.id)
+    end
   end
 end
